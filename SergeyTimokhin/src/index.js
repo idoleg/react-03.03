f5be3025cd3410ef92f76dc57f5b1d9caa8591a3
SergioPuff <seryozhapuff@gmail.com>
2020-03-09 21:24:00 +0200

    добавлен каунтер

--- SergeyTimokhin/src/index.js
+++ SergeyTimokhin/src/index.js
@@ -1,26 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { App } from "./components/App";
+
+// setTimeout(() => {
+//     console.log(Test)
+// }, 2000)
+
+// const messages = [
+//     {name: "Boby", content: "Hello, World!"},
+//     {name: "Dilan", content: "Hello, how are you?"},
+//     {name: "Boby", content: "I'm good, thanks"},
+// ]
+
+// const Message = ({name, content}) => {
+//     return <li><strong>{name}:</strong> {content}</li>
+// }
+
+// const MessageList = ({messages}) => {
+//     return (<ul>
+//         {messages.map((item, index) => <Message {...item} key={index} />)}
+//     </ul>)
+// }
+
+// const Messenger = ({messages}) => {
+//     return (<div>
+//         <MessageList messages={messages} />
+//         <button>Send message</button>
+//     </div>)
+// }
 
 
-// const element = React.createElement("a", { href: "https://ya.ru" }, "Yandex");
-// const Link = (props) => React.createElement("a", { href: "https://" + props.to }, props.to.toUpperCase());
-
-
-const messages = [
-    {name: "Boby", content: "Hello, World!"},
-    {name: "Dilan", content: "Hello, how are you?"},
-    {name: "Boby", content: "i'm good, thanks"},
-]
-
-const Message = ({name, content}) => {
-    return <li><strong>{name}</strong>: {content}</li>
-}
-
-const MessageList = ({messages}) => {
-    return (<ul>
-        {messages.map((item, index) => <Message {...item} key={index} />)}
-    </ul>)
-}
-
-
-ReactDOM.render(<MessageList messages={messages}/>, document.getElementById("root"));
+ReactDOM.render( <App /> , document.getElementById("root"));
