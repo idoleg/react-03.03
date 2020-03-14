9e40aeb775a94f89f50f1b6ae4c6c0a45b10c9d9
hordydmitry <hordydmitry@gmail.com>
2020-03-14 16:10:48 +0500

    settings and scss tests are done

--- DmitryBachurin/src/components/MessageField/Message/Message.jsx
+++ DmitryBachurin/src/components/MessageField/Message/Message.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import classname from 'classname';
 
-const Message = ({name, content}) => {
-    return <li><strong>{name}:</strong> {content}</li>
+const Message = ({ name, content }) => {
+
+    const className = classname({
+        'message': true,
+        'message__robot': name == 'Robot' ? true : false,
+    })
+    return <li className={className}><strong>{name}:</strong> {content}</li>
 };
 
 export { Message };
