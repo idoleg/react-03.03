236e3c29665c9491c90d0e9784f32c4044019815
SergioPuff <seryozhapuff@gmail.com>
2020-03-20 12:45:05 +0200

    изменен веб пак

--- SergeyTimokhin/webpack.config.js
+++ SergeyTimokhin/webpack.config.js
@@ -1,5 +1,43 @@
+// const path = require("path");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+// module.exports = {
+//     entry: path.resolve(__dirname, "src", "index.js"),
+//     output: {
+//         path: path.resolve(__dirname, "dist"),
+//         filename: "index.js",
+//     },
+//     module: {
+//         rules: [{
+//                 test: /\.(js|jsx)$/i,
+//                 include: path.resolve(__dirname, "src"),
+//                 loader: 'babel-loader',
+//                 options: {
+//                     presets: ['@babel/env', '@babel/react'],
+//                     plugins: ['@babel/plugin-proposal-class-properties']
+//                 }
+//             },
+//             {
+//                 test: /\.css$/i,
+//                 use: ['style-loader', 'css-loader']
+//             }
+//         ]
+//     },
+//     plugins: [
+//         new HtmlWebpackPlugin({ template: path.resolve(__dirname, "src", "index.html") })
+//     ],
+//     resolve: {
+//         extensions: [".jsx", ".js"],
+//     },
+//     devServer: {
+//         historyApiFallback: true,
+//     }
+// }
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HTMLInlineCSSWebpackPlugin = require("html-inline-css-webpack-plugin").default;
+
 
 module.exports = {
     entry: path.resolve(__dirname, "src", "index.js"),
@@ -19,17 +57,20 @@ module.exports = {
             },
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
             }
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({ template: path.resolve(__dirname, "src", "index.html") })
+        new HtmlWebpackPlugin({ template: path.resolve(__dirname, "src", "index.html") }),
+        new MiniCssExtractPlugin(),
+        new HTMLInlineCSSWebpackPlugin()
     ],
     resolve: {
         extensions: [".jsx", ".js"],
     },
     devServer: {
         historyApiFallback: true,
-    }
+    },
+    devtool: 'inline-source-map'
 }
