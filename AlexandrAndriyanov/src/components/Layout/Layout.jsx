eebc1c6cd9f6054e3751925e9858dcfa9f6c5117
Wader488 <alexag488@gmail.com>
2020-03-22 21:08:59 +0300

    Lesson 4 Alexandr Andriyanov

--- AlexandrAndriyanov/src/components/Layout/Layout.jsx
+++ AlexandrAndriyanov/src/components/Layout/Layout.jsx
@@ -3,9 +3,6 @@ import {Header} from "../Header/Header";
 import {ChatList} from "../ChatList/ChatList";
 import {MessageField} from "../MessageField/MessageField";
 
-
-
-
 export const Layout = () => {
     return null;
 };
