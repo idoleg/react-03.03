229d9b9747887757d77cc5f1c3c0e82b0e65e469
deepar57 <deepar@yandex.ru>
2020-03-27 20:01:20 +0300

    Lesson 06

--- ParkhomenkoDA/src/App.jsx
+++ ParkhomenkoDA/src/App.jsx
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import ChatContainer from './containers/ChatContainer';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import {ChatList} from './components/ChatList/ChatList'
-import {initStore} from './store'
+//import {ChatList} from './components/ChatList/ChatList'
+import {initStore, history} from './store'
 import {Provider} from 'react-redux'
-import {initChats} from './store/chatActions'
+import {initChats, sendMessage} from './store/chatActions'
+import ChatListContainer from './containers/ChatListContainer';
+import {ConnectedRouter} from 'connected-react-router';
 
 const store = initStore();
 store.dispatch(initChats());
@@ -12,11 +14,11 @@ store.dispatch(initChats());
 export const App = () => {
     return (
         <Provider store={store}>
-            <BrowserRouter>
+            <ConnectedRouter history={history}>
                 <Switch>
                     <Route path="/" exact>It's index page</Route>
                     <Route path="/chats"> 
-                        <ChatList/>
+                        <ChatListContainer/>
                         <Switch>
                             <Route path="/chats" exact component={ChatContainer} />
                             <Route path="/chats/:id" exact component={ChatContainer} />
@@ -26,7 +28,7 @@ export const App = () => {
                     <Route path="/contacts">It's contacts page</Route>
                     <Route path="/">It's 404 page. Not found.</Route>
                 </Switch>
-            </BrowserRouter>
+            </ConnectedRouter>
         </Provider>
     )
 }
