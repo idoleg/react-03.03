1f93b5a7bd1457571c82e7d14071e66246e9edf3
senior012 <senior012@gmail.com>
2020-03-30 03:29:05 +0300

    Урок 5

--- Konstantin_Grigoryevyh/static_src/App.jsx
+++ Konstantin_Grigoryevyh/static_src/App.jsx
@@ -1,32 +1,41 @@
 import React from 'react';
-import {ChatContainer} from './containers/ChatContainer/ChatContainer.jsx';
+import ChatContainer from './containers/ChatContainer/ChatContainer.jsx';
 import {Header} from './components/Header/Header';
 import {Footer} from './components/Footer/Footer';
 import {ChatList} from "./components/ChatList/ChatList";
 import {Profile} from "./components/Profile/Profile";
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {initStore} from "./store/index.js";
+import {Provider} from  'react-redux';
+import {initChats} from "./store/chatActions";
+
 import './styles/styles.css';
+
+const store = initStore();
+store.dispatch(initChats());
 
 export const App = () => {
     return (
-                <BrowserRouter>
-                    <Switch>
-                        <Route path="/" exact> It;s index page</Route>
-                        <Route path="/about">it's about page</Route>
-                        <Route path="/profile"exact component={Profile} />
-                        <Route path="/chats/" >
-                            <Header />
-                            <div className="chat">
-                                <ChatList />
-                                <Switch>
-                                    <Route path="/chats" exact component={ChatContainer} />
-                                    <Route path="/chats/:id" exact component={ChatContainer} />
-                                </Switch>
-                            </div>
-                            <Footer />
-                        </Route>
-                        <Route path="/" >it's 404 page</Route>
-                    </Switch>
-                </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" exact> It;s index page</Route>
+                    <Route path="/about">it's about page</Route>
+                    <Route path="/profile"exact component={Profile} />
+                    <Route path="/chats/" >
+                        <Header />
+                        <div className="chat">
+                            <ChatList />
+                            <Switch>
+                                <Route path="/chats" exact component={ChatContainer} />
+                                <Route path="/chats/:id" exact component={ChatContainer} />
+                            </Switch>
+                        </div>
+                        <Footer />
+                    </Route>
+                    <Route path="/" >it's 404 page</Route>
+                </Switch>
+            </BrowserRouter>
+        </Provider>
     )
 }
--- Konstantin_Grigoryevyh/static_src/containers/ChatContainer/ChatContainer.jsx
+++ Konstantin_Grigoryevyh/static_src/containers/ChatContainer/ChatContainer.jsx
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
 import {Chat} from '../../components/Chat/Chat';
+import {connect} from 'react-redux'
+import {bindActionCreators} from "redux";
+import {sendMessage} from '../../store/chatActions';
+
 
 export const ROBOT = 'Robot';
 
-export class ChatContainer extends Component {
-    state = {
+/* export class ChatContainer extends Component {
+   state = {
         chats: {
             1: {
                 name: 'Chat 1',
@@ -66,4 +70,32 @@ export class ChatContainer extends Component {
 
         return <Chat messages={messages} onSendMessage={this.handleSendMessage(id)} />;
     }
+}*/
+
+const mapStateToProps = (store, props) => {
+    const {id} = props.match.params;
+    const chats = id && store.chats ? store.chats  : undefined
+
+    return {
+        messages: chats[id] ? chats[id].messages : undefined,
+    }
+
 }
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+    sendMessage
+}, dispatch)
+
+const mergeProps = (stateProps, dispatchProps, ownProps) => {
+    const {id} = ownProps.match.params;
+
+    const onSendMessage = ({name, content}) => {
+        dispatchProps.sendMessage(id, name, content)
+    }
+
+    return {
+        messages: stateProps.messages,
+        onSendMessage
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(Chat);
