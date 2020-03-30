39ab00a09a9d92a996f3f8d2ae0df083de7e62ff
Vladislav Vorobyov <vladislav.vorobyov@gmail.com>
2020-03-30 20:34:16 +0600

    Add redux-persist

--- VladislavVorobyov/src/App.jsx
+++ VladislavVorobyov/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Switch, Route, Link} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {ConnectedRouter} from "connected-react-router";
+import {PersistGate} from 'redux-persist/integration/react';
 import initStore, { history } from "Utils/store";
 import {initChats} from "Actions/chatActions";
 import {initUsers} from "Actions/usersAction";
@@ -9,7 +10,7 @@ import {initMessages} from "Actions/messageActions";
 import {Messenger} from "Components/Messenger/Messenger";
 import ProfileContainer from "Containers/ProfileContainer";
 
-const store = initStore();
+const {store, persistor} = initStore();
 store.dispatch(initChats());
 store.dispatch(initUsers());
 store.dispatch(initMessages());
@@ -20,15 +21,17 @@ store.dispatch(initMessages());
 export const App = (props) => {
     return (
         <Provider store={store} >
-            <ConnectedRouter history={history}>
-                <Switch>
-                    <Route path="/" exact >
-                        <Link to="/chats">Перейти к чатам</Link>
-                    </Route>
-                    <Route path="/chats" component={Messenger} />
-                    <Route path="/profile/" exact component={ProfileContainer}/>
-                </Switch>
-            </ConnectedRouter>
+            <PersistGate loading={null} persistor={persistor} >
+                <ConnectedRouter history={history}>
+                    <Switch>
+                        <Route path="/" exact >
+                            <Link to="/chats">Перейти к чатам</Link>
+                        </Route>
+                        <Route path="/chats" component={Messenger} />
+                        <Route path="/profile/" exact component={ProfileContainer}/>
+                    </Switch>
+                </ConnectedRouter>
+            </PersistGate>
         </Provider>
     )
 };
--- VladislavVorobyov/src/utils/store.js
+++ VladislavVorobyov/src/utils/store.js
@@ -1,16 +1,26 @@
 import {createStore, applyMiddleware, compose} from 'redux';
 import {createBrowserHistory} from "history";
 import {routerMiddleware} from "connected-react-router";
+import {persistStore, persistReducer} from "redux-persist";
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 import initReducer from 'Reducers';
 import middlewares from 'Middlewares';
+
+const persistConfig = {
+    key: 'geekmessanger',
+    storage,
+    stateReconciler: autoMergeLevel2,
+    whitelist: ['chats', 'messages'],
+};
 
 export const history = createBrowserHistory();
 
 function initStore() {
     const initialStore = {};
     const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-    return createStore(
-        initReducer(history),
+    const store = createStore(
+        persistReducer(persistConfig, initReducer(history)),
         initialStore,
         composeEnhancers(
             applyMiddleware(
@@ -19,6 +29,8 @@ function initStore() {
             ),
         )
     );
+    const persistor = persistStore(store);
+    return {store, persistor};
 }
 
 export default initStore;
