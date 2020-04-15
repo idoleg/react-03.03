f942e21c85c728e0f4475e5e9154ee1a798e4234
nikut <nikutjev.dima111@gmail.com>
2020-04-15 19:54:30 +0300

    implemented PWA

--- DmitryNikutev/src/utils/Constants.js
+++ DmitryNikutev/src/utils/Constants.js
@@ -6,13 +6,8 @@ export const USER_API_URL = "/api/user.json";
 export const BOT_API_URL = "/api/bot/";
 
 export const BOT_NAME = "Bot";
-export const BOT_RESPONSE = "Thank you, ";
 export const BOT_GREETING = "Greetings!";
 export const BOT_RESPONSE_DELAY = 1000;
-
-export const DEFAULT_USERNAME = "Guest";
-
-export const ACTION_LOCATION_CHANGE = "@@router/LOCATION_CHANGE";
 
 //loading statuses
 export const FETCHING = "fetching";
